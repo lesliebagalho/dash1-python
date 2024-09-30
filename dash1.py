5ac95f95eca3dc4205981b8d53c33479313040b2
Leslie Bagalho <leslie.bagalho@yahoo.com.br>
2024-09-29 23:19:03 -0300

    Linha 7

--- dash1.py
+++ dash1.py
@@ -2,4 +2,6 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
+st.set_page_config(layout="wide")
 df = pd.read_csv("supermarket_sales.csv", sep=";", decimal=",")
+df["Date"] = pd.to_datetime(df["Date"])
