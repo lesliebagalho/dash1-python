35a4a00e11feef0078e3884a202378764f134156
Leslie Bagalho <leslie.bagalho@yahoo.com.br>
2024-09-29 23:23:00 -0300

    plotly

--- dash1.py
+++ dash1.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
+# import plotly.express as px
 
 st.set_page_config(layout="wide")
 df = pd.read_csv("supermarket_sales.csv", sep=";", decimal=",")
